e7bf4a05d9e1867b57ccb06877c0df557cc8bf0a
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-23 11:58:01 -0800

    added logo to signup page

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static("views/assets"));
 
 // Set Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
